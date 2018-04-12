f1031a03231b23c0edf0ac5fa1fed555807a3fcc
Logan Sorese <logan@sorese.com>
2018-04-11 17:30:34 -0700

    4/11

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,41 @@
-jQuery(function() {
-  jQuery('audio').mediaelementplayer({
+$(function() {
+  $('audio').mediaelementplayer({
   	features: ['playpause','progress','current','tracks','fullscreen']
   });
   $('.about__circle').click( function() { // Execute when the toggle button is clicked.
     $('.about__content').slideToggle();
   });
+
+  $('.close').on('click', function(){
+    $(this).parent().fadeTo("slow" , 0, function(){
+      $(this).slideUp( "slow");
+    });
+    console.log('clicked');
+  })
+
+  $('.list.sort .list__item').on('click', function(){
+    $('.list.sort .list__item').removeClass('active');
+    $(this).addClass( "active" )
+  })
+
+  var $btns = $('.sorter').click(function() {
+
+    if ($(this).data('filter') == 'all') {
+      $('.split__left .box').fadeIn(450);
+      $('.filter').text('');
+    } else {
+      $('.filter').text('Filtering for ' + $(this).text() + '.');
+      $('.split__left .box').hide();
+      $filter = $(this).data('filter');
+      console.log($filter);
+      $('.split__left .box').each(function(){
+        if ($(this).data('filter') == $filter) {
+          $(this).fadeIn(450);
+        }
+      })
+
+    }
+    $btns.removeClass('active');
+    $(this).addClass('active');
+  })
 });
