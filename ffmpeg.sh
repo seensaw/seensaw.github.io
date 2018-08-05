1545d8e13127e04deba6602f687d52c8b7f340d8
LGBR <lgbr@LGBRs-MacBook-Pro.local>
2018-08-04 21:58:24 -0700

    8/04

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,3 +1,5 @@
-ffmpeg -loop 1 -i /Users/lgbr/Desktop/seensaw_square.png -i /Users/lgbr/Documents/seensaw-dev/docs/audio/week27_freeknittingclasspt2.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest /Users/lgbr/Desktop/square_out.mp4
-
-ffmpeg -loop 1 -i /Users/lgbr/Desktop/seensaw_narrow.png -i /Users/lgbr/Documents/seensaw-dev/docs/audio/week27_freeknittingclasspt1.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest /Users/lgbr/Desktop/narrow_out.mp4
+#!/bin/bash
+echo MP3 Filename?
+read mp3
+ffmpeg -loop 1 -i /Users/lgbr/Desktop/seensaw_narrow.png -i /Users/lgbr/Documents/seensaw-dev/docs/audio/$mp3 -pix_fmt yuv420p -c:v libx264 -c:a aac -shortest /Users/lgbr/Desktop/narrow_out.mp4
+ffmpeg -loop 1 -i /Users/lgbr/Desktop/seensaw_square.png -i /Users/lgbr/Documents/seensaw-dev/docs/audio/$mp3 -pix_fmt yuv420p -c:v libx264 -c:a aac -shortest /Users/lgbr/Desktop/square_out.mp4
